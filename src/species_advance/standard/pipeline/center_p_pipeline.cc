c85033144c65902b5b60dd7003af8b2a36609107
Dave Nystrom <wdn@lanl.gov>
2019-05-21 19:32:39 -0600

    Add AoSoA implementations for the center_p_pipeline functions.

--- src/species_advance/standard/pipeline/center_p_pipeline.cc
+++ src/species_advance/standard/pipeline/center_p_pipeline.cc
@@ -19,7 +19,93 @@ center_p_pipeline_scalar( center_p_pipeline_args_t * args,
                           int pipeline_rank,
                           int n_pipeline )
 {
-  ERROR(("Need AoSoA implementation."));
+  const interpolator_t * ALIGNED(128) f0 = args->f0;
+
+  particle_block_t     * ALIGNED(32)  pb;
+
+  const interpolator_t * ALIGNED(16)  f;
+
+  const float qdt_2mc        =     args->qdt_2mc;
+  const float qdt_4mc        = 0.5*args->qdt_2mc; // For half Boris rotate
+  const float one            = 1.0;
+  const float one_third      = 1.0/3.0;
+  const float two_fifteenths = 2.0/15.0;
+
+  float dx, dy, dz, ux, uy, uz;
+  float hax, hay, haz, cbx, cby, cbz;
+  float v0, v1, v2, v3, v4;
+  int   ii;
+
+  int first, n;
+
+  // Determine which particles this pipeline processes.
+
+  DISTRIBUTE( args->np, PARTICLE_BLOCK_SIZE, pipeline_rank, n_pipeline, first, n );
+
+  pb = args->pb0 + first / PARTICLE_BLOCK_SIZE;
+
+  // Process particles for this pipeline.
+
+  int ib = 0;
+  int ip = 0;
+
+//for( ; n; n--, p++ )
+  #ifdef VPIC_SIMD_LEN
+  #pragma omp simd simdlen(VPIC_SIMD_LEN)
+  #endif
+  for( int i = 0 ; i < n; i++ )
+  {
+    ib   = i / PARTICLE_BLOCK_SIZE;          // Index of particle block.
+    ip   = i - PARTICLE_BLOCK_SIZE * ib;     // Index of next particle in block.
+
+    dx   = pb[ib].dx[ip];                    // Load position
+    dy   = pb[ib].dy[ip];
+    dz   = pb[ib].dz[ip];
+    ii   = pb[ib].i [ip];
+
+    f    = f0 + ii;                          // Interpolate E
+
+    hax  = qdt_2mc*(    ( f->ex    + dy*f->dexdy    ) +
+                     dz*( f->dexdz + dy*f->d2exdydz ) );
+
+    hay  = qdt_2mc*(    ( f->ey    + dz*f->deydz    ) +
+                     dx*( f->deydx + dz*f->d2eydzdx ) );
+
+    haz  = qdt_2mc*(    ( f->ez    + dx*f->dezdx    ) +
+                     dy*( f->dezdy + dx*f->d2ezdxdy ) );
+
+    cbx  = f->cbx + dx*f->dcbxdx;            // Interpolate B
+    cby  = f->cby + dy*f->dcbydy;
+    cbz  = f->cbz + dz*f->dcbzdz;
+
+    ux   = pb[ib].ux[ip];                    // Load momentum
+    uy   = pb[ib].uy[ip];
+    uz   = pb[ib].uz[ip];
+
+    ux  += hax;                              // Half advance E
+    uy  += hay;
+    uz  += haz;
+
+    v0   = qdt_4mc/(float)sqrt(one + (ux*ux + (uy*uy + uz*uz)));
+    /**/                                     // Boris - scalars
+    v1   = cbx*cbx + (cby*cby + cbz*cbz);
+    v2   = (v0*v0)*v1;
+    v3   = v0*(one+v2*(one_third+v2*two_fifteenths));
+    v4   = v3/(one+v1*(v3*v3));
+    v4  += v4;
+
+    v0   = ux + v3*( uy*cbz - uz*cby );      // Boris - uprime
+    v1   = uy + v3*( uz*cbx - ux*cbz );
+    v2   = uz + v3*( ux*cby - uy*cbx );
+
+    ux  += v4*( v1*cbz - v2*cby );           // Boris - rotation
+    uy  += v4*( v2*cbx - v0*cbz );
+    uz  += v4*( v0*cby - v1*cbx );
+
+    pb[ib].ux[ip] = ux;                      // Store momentum
+    pb[ib].uy[ip] = uy;
+    pb[ib].uz[ip] = uz;
+  }
 }
 #else // VPIC_USE_AOSOA_P is not defined i.e. VPIC_USE_AOS_P case.
 void
@@ -270,7 +356,26 @@ void
 center_p_pipeline( species_t * RESTRICT sp,
                    const interpolator_array_t * RESTRICT ia )
 {
-  ERROR(("Need AoSoA implementation."));
+  DECLARE_ALIGNED_ARRAY( center_p_pipeline_args_t, 128, args, 1 );
+
+  if ( !sp ||
+       !ia ||
+       sp->g != ia->g )
+  {
+    ERROR( ( "Bad args." ) );
+  }
+
+  // Have the pipelines do the bulk of particles in blocks and have the
+  // host do the final incomplete block.
+
+  args->pb0     = sp->pb;
+  args->f0      = ia->i;
+  args->qdt_2mc = ( sp->q * sp->g->dt ) / ( 2 * sp->m * sp->g->cvac );
+  args->np      = sp->np;
+
+  EXEC_PIPELINES( center_p, args, 0 );
+
+  WAIT_PIPELINES();
 }
 #else // VPIC_USE_AOSOA_P is not defined i.e. VPIC_USE_AOS_P case.
 void
--- src/species_advance/standard/pipeline/center_p_pipeline_v4.cc
+++ src/species_advance/standard/pipeline/center_p_pipeline_v4.cc
@@ -12,7 +12,139 @@ center_p_pipeline_v4( center_p_pipeline_args_t * args,
                       int pipeline_rank,
                       int n_pipeline )
 {
-  ERROR( ( "Need AoSoA implementation." ) );
+  const interpolator_t * ALIGNED(128) f0 = args->f0;
+
+  particle_block_t     * ALIGNED(128) pb;
+
+  const float          * ALIGNED(16)  vp00;
+  const float          * ALIGNED(16)  vp01;
+  const float          * ALIGNED(16)  vp02;
+  const float          * ALIGNED(16)  vp03;
+
+  const v4float qdt_2mc(    args->qdt_2mc);
+  const v4float qdt_4mc(0.5*args->qdt_2mc); // For half Boris rotate.
+  const v4float one(1.0);
+  const v4float one_third(1.0/3.0);
+  const v4float two_fifteenths(2.0/15.0);
+
+  v4float dx, dy, dz, ux, uy, uz, q;
+  v4float hax, hay, haz, cbx, cby, cbz;
+  v4float v00, v01, v02, v03, v04, v05;
+  v4int   ii;
+
+  int itmp, nq;
+
+  // Determine which particle blocks this pipeline processes.
+
+  DISTRIBUTE( args->np, PARTICLE_BLOCK_SIZE, pipeline_rank, n_pipeline, itmp, nq );
+
+  pb = args->pb0 + itmp / PARTICLE_BLOCK_SIZE;
+
+  nq >>= 2;
+
+  // Process the particle blocks for this pipeline.  For the initial AoSoA version,
+  // assume that PARTICLE_BLOCK_SIZE is the same as the vector length.  A future
+  // version might allow PARTICLE_BLOCK_SIZE to be an integral multiple of the vector
+  // length.
+
+  for( ; nq; nq--, pb++ )
+  {
+    //--------------------------------------------------------------------------
+    // Load particle position data.
+    //--------------------------------------------------------------------------
+    load_4x1( &pb[0].dx[0], dx );
+    load_4x1( &pb[0].dy[0], dy );
+    load_4x1( &pb[0].dz[0], dz );
+    load_4x1( &pb[0].i [0], ii );
+
+    //--------------------------------------------------------------------------
+    // Set field interpolation pointers.
+    //--------------------------------------------------------------------------
+    vp00 = ( const float * ALIGNED(16) ) ( f0 + ii(0) );
+    vp01 = ( const float * ALIGNED(16) ) ( f0 + ii(1) );
+    vp02 = ( const float * ALIGNED(16) ) ( f0 + ii(2) );
+    vp03 = ( const float * ALIGNED(16) ) ( f0 + ii(3) );
+
+    //--------------------------------------------------------------------------
+    // Load interpolation data for particles.
+    //--------------------------------------------------------------------------
+    load_4x4_tr( vp00, vp01, vp02, vp03,
+		 hax, v00, v01, v02 );
+
+    hax = qdt_2mc*fma( fma( dy, v02, v01 ), dz, fma( dy, v00, hax ) );
+
+    //--------------------------------------------------------------------------
+    // Load interpolation data for particles.
+    //--------------------------------------------------------------------------
+    load_4x4_tr( vp00+4, vp01+4, vp02+4, vp03+4,
+		 hay, v03, v04, v05 );
+
+    hay = qdt_2mc*fma( fma( dz, v05, v04 ), dx, fma( dz, v03, hay ) );
+
+    //--------------------------------------------------------------------------
+    // Load interpolation data for particles.
+    //--------------------------------------------------------------------------
+    load_4x4_tr( vp00+8, vp01+8, vp02+8, vp03+8,
+		 haz, v00, v01, v02 );
+
+    haz = qdt_2mc*fma( fma( dx, v02, v01 ), dy, fma( dx, v00, haz ) );
+
+    //--------------------------------------------------------------------------
+    // Load interpolation data for particles.
+    //--------------------------------------------------------------------------
+    load_4x4_tr( vp00+12, vp01+12, vp02+12, vp03+12,
+		 cbx, v03, cby, v04 );
+
+    cbx = fma( v03, dx, cbx );
+    cby = fma( v04, dy, cby );
+
+    //--------------------------------------------------------------------------
+    // Load interpolation data for particles, final.
+    //--------------------------------------------------------------------------
+    load_4x2_tr( vp00+16, vp01+16, vp02+16, vp03+16,
+		 cbz, v05 );
+
+    cbz = fma( v05, dz, cbz );
+
+    //--------------------------------------------------------------------------
+    // Load particle momentum data.
+    //--------------------------------------------------------------------------
+    load_4x1( &pb[0].ux[0], ux );
+    load_4x1( &pb[0].uy[0], uy );
+    load_4x1( &pb[0].uz[0], uz );
+
+    // load_4x4_tr( &p[0].ux, &p[1].ux, &p[2].ux, &p[3].ux,
+    //              ux, uy, uz, q );
+
+    //--------------------------------------------------------------------------
+    // Update momentum.
+    //--------------------------------------------------------------------------
+    ux  += hax;
+    uy  += hay;
+    uz  += haz;
+
+    v00  = qdt_4mc * rsqrt( one + fma( ux, ux, fma( uy, uy, uz * uz ) ) );
+    v01  = fma( cbx, cbx, fma( cby, cby, cbz * cbz ) );
+    v02  = ( v00 * v00 ) * v01;
+    v03  = v00 * fma( v02, fma( v02, two_fifteenths, one_third ), one );
+    v04  = v03 * rcp( fma( v03 * v03, v01, one ) );
+    v04 += v04;
+
+    v00  = fma( fms( uy, cbz, uz * cby ), v03, ux );
+    v01  = fma( fms( uz, cbx, ux * cbz ), v03, uy );
+    v02  = fma( fms( ux, cby, uy * cbx ), v03, uz );
+
+    ux   = fma( fms( v01, cbz, v02 * cby ), v04, ux );
+    uy   = fma( fms( v02, cbx, v00 * cbz ), v04, uy );
+    uz   = fma( fms( v00, cby, v01 * cbx ), v04, uz );
+
+    //--------------------------------------------------------------------------
+    // Store particle momentum data.
+    //--------------------------------------------------------------------------
+    store_4x1( ux, &pb[0].ux[0] );
+    store_4x1( uy, &pb[0].uy[0] );
+    store_4x1( uz, &pb[0].uz[0] );
+  }
 }
 #else // VPIC_USE_AOSOA_P is not defined i.e. VPIC_USE_AOS_P case.
 void
