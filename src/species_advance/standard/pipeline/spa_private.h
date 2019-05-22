a69419ebeb5e483c85d9d1b8369e9ebe9be7e664
Dave Nystrom <wdn@lanl.gov>
2019-05-22 13:33:17 -0600

    Add species pointer to member data for center_p_pipeline_args for AoSoA implementation.

--- src/species_advance/standard/pipeline/spa_private.h
+++ src/species_advance/standard/pipeline/spa_private.h
@@ -101,10 +101,11 @@ typedef struct center_p_pipeline_args
 {
   MEM_PTR( particle_block_t,     128 ) pb0;     // Particle block array
   MEM_PTR( const interpolator_t, 128 ) f0;      // Interpolator array
+  MEM_PTR( const species_t,      1   ) sp;      // Species pointer
   float                                qdt_2mc; // Particle/field coupling
   int                                  np;      // Number of particles
 
-  PAD_STRUCT( 2*SIZEOF_MEM_PTR + sizeof(float) + sizeof(int) )
+  PAD_STRUCT( 3*SIZEOF_MEM_PTR + sizeof(float) + sizeof(int) )
 } center_p_pipeline_args_t;
 #else
 typedef struct center_p_pipeline_args
