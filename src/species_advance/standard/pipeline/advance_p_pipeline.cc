2efdc5a68d9afdc3dcfafa2e79f48e0575a16fef
Dave Nystrom <wdn@lanl.gov>
2019-06-02 21:51:59 -0600

    Implement a strategy where particles which need processing by move_p are first stored in the particle mover array. Then, they are processed by move_p later after all the particles in all of the cells or voxels have been processed.

--- src/species_advance/standard/pipeline/advance_p_pipeline.cc
+++ src/species_advance/standard/pipeline/advance_p_pipeline.cc
@@ -603,24 +603,33 @@ advance_p_pipeline_scalar( advance_p_pipeline_args_t * args,
 
         else                                            // Unlikely
         {
-          local_pm->dispx = ux;
-          local_pm->dispy = uy;
-          local_pm->dispz = uz;
-
-          local_pm->i     = p - p0;
-
-          if ( move_p( p0, local_pm, a0, g, qsp, sp ) ) // Unlikely
-          {
-            if ( nm < max_nm )
-            {
-              pm[nm++] = local_pm[0];
-            }
-
-            else
-            {
-              itmp++;                                   // Unlikely
-            }
-          }
+          // Load and store particle info in the particle mover array to be
+          // processed by move_p later.
+          pm[nm].dispx = ux;
+          pm[nm].dispy = uy;
+          pm[nm].dispz = uz;
+          pm[nm].i     = p - p0;
+          nm++;
+          //--------------------------------------------------------------------
+          // local_pm->dispx = ux;
+          // local_pm->dispy = uy;
+          // local_pm->dispz = uz;
+          //
+          // local_pm->i     = p - p0;
+          //
+          // if ( move_p( p0, local_pm, a0, g, qsp, sp ) ) // Unlikely
+          // {
+          //   if ( nm < max_nm )
+          //   {
+          //     pm[nm++] = local_pm[0];
+          //   }
+          //
+          //   else
+          //   {
+          //     itmp++;                                   // Unlikely
+          //   }
+          // }
+	  //--------------------------------------------------------------------
         }
       }
     }
@@ -635,201 +644,27 @@ advance_p_pipeline_scalar( advance_p_pipeline_args_t * args,
                 sp->g->nz );
   }
 
-  #if 0
-  float dx, dy, dz, ux, uy, uz, q;
-  float hax, hay, haz, cbx, cby, cbz;
-  float v0, v1, v2, v3, v4, v5;
-  int   ii;
-
-  int itmp, n, nm, max_nm;
-
-  DECLARE_ALIGNED_ARRAY( particle_mover_t, 16, local_pm, 1 );
-
-  // Determine which quads of particles quads this pipeline processes.
-
-  DISTRIBUTE( args->np, 16, pipeline_rank, n_pipeline, itmp, n );
-
-  p = args->p0 + itmp;
-
-  // Determine which movers are reserved for this pipeline.
-  // Movers (16 bytes) should be reserved for pipelines in at least
-  // multiples of 8 such that the set of particle movers reserved for
-  // a pipeline is 128-byte aligned and a multiple of 128-byte in
-  // size.  The host is guaranteed to get enough movers to process its
-  // particles with this allocation.
-
-  max_nm = args->max_nm - ( args->np&15 );
-
-  if ( max_nm < 0 ) max_nm = 0;
-
-  DISTRIBUTE( max_nm, 8, pipeline_rank, n_pipeline, itmp, max_nm );
-
-  if ( pipeline_rank == n_pipeline ) max_nm = args->max_nm - itmp;
-
-  pm   = args->pm + itmp;
-  nm   = 0;
-  itmp = 0;
-
-  // Determine which accumulator array to use
-  // The host gets the first accumulator array.
-
-  if ( pipeline_rank != n_pipeline )
+  // Process the particle mover array with move_p.
+  int nm_move_p = nm;
+  nm = 0;
+  for( ; nm_move_p; nm_move_p-- )
   {
-    a0 += ( 1 + pipeline_rank ) *
-          POW2_CEIL( (args->nx+2)*(args->ny+2)*(args->nz+2), 2 );
-  }
-
-  // Process particles for this pipeline.
-
-  for( ; n; n--, p++ )
-  {
-    dx   = p->dx;                             // Load position
-    dy   = p->dy;
-    dz   = p->dz;
-    ii   = p->i;
-
-    f    = f0 + ii;                           // Interpolate E
-
-    hax  = qdt_2mc*(    ( f->ex    + dy*f->dexdy    ) +
-                     dz*( f->dexdz + dy*f->d2exdydz ) );
-
-    hay  = qdt_2mc*(    ( f->ey    + dz*f->deydz    ) +
-                     dx*( f->deydx + dz*f->d2eydzdx ) );
-
-    haz  = qdt_2mc*(    ( f->ez    + dx*f->dezdx    ) +
-                     dy*( f->dezdy + dx*f->d2ezdxdy ) );
-
-    cbx  = f->cbx + dx*f->dcbxdx;             // Interpolate B
-    cby  = f->cby + dy*f->dcbydy;
-    cbz  = f->cbz + dz*f->dcbzdz;
-
-    ux   = p->ux;                             // Load momentum
-    uy   = p->uy;
-    uz   = p->uz;
-    q    = p->w;
-
-    ux  += hax;                               // Half advance E
-    uy  += hay;
-    uz  += haz;
-
-    v0   = qdt_2mc / sqrtf( one + ( ux*ux + ( uy*uy + uz*uz ) ) );
-
-                                              // Boris - scalars
-    v1   = cbx*cbx + ( cby*cby + cbz*cbz );
-    v2   = ( v0*v0 ) * v1;
-    v3   = v0 * ( one + v2 * ( one_third + v2 * two_fifteenths ) );
-    v4   = v3 / ( one + v1 * ( v3 * v3 ) );
-    v4  += v4;
-
-    v0   = ux + v3*( uy*cbz - uz*cby );       // Boris - uprime
-    v1   = uy + v3*( uz*cbx - ux*cbz );
-    v2   = uz + v3*( ux*cby - uy*cbx );
-
-    ux  += v4*( v1*cbz - v2*cby );            // Boris - rotation
-    uy  += v4*( v2*cbx - v0*cbz );
-    uz  += v4*( v0*cby - v1*cbx );
-
-    ux  += hax;                               // Half advance E
-    uy  += hay;
-    uz  += haz;
-
-    p->ux = ux;                               // Store momentum
-    p->uy = uy;
-    p->uz = uz;
-
-    v0   = one / sqrtf( one + ( ux*ux+ ( uy*uy + uz*uz ) ) );
-                                              // Get norm displacement
-
-    ux  *= cdt_dx;
-    uy  *= cdt_dy;
-    uz  *= cdt_dz;
-
-    ux  *= v0;
-    uy  *= v0;
-    uz  *= v0;
-
-    v0   = dx + ux;                           // Streak midpoint (inbnds)
-    v1   = dy + uy;
-    v2   = dz + uz;
-
-    v3   = v0 + ux;                           // New position
-    v4   = v1 + uy;
-    v5   = v2 + uz;
-
-    // FIXME-KJB: COULD SHORT CIRCUIT ACCUMULATION IN THE CASE WHERE QSP==0!
-    if (  v3 <= one &&  v4 <= one &&  v5 <= one &&   // Check if inbnds
-         -v3 <= one && -v4 <= one && -v5 <= one )
+    if ( move_p( p0, &pm[ nm_move_p - 1 ], a0, g, qsp, sp ) ) // Unlikely
     {
-      // Common case (inbnds).  Note: accumulator values are 4 times
-      // the total physical charge that passed through the appropriate
-      // current quadrant in a time-step.
-
-      q *= qsp;
-
-      p->dx = v3;                             // Store new position
-      p->dy = v4;
-      p->dz = v5;
-
-      dx = v0;                                // Streak midpoint
-      dy = v1;
-      dz = v2;
-
-      v5 = q*ux*uy*uz*one_third;              // Compute correction
-
-      a  = (float *)( a0 + ii );              // Get accumulator
-
-      #define ACCUMULATE_J(X,Y,Z,offset)                                \
-      v4  = q*u##X;   /* v2 = q ux                            */        \
-      v1  = v4*d##Y;  /* v1 = q ux dy                         */        \
-      v0  = v4-v1;    /* v0 = q ux (1-dy)                     */        \
-      v1 += v4;       /* v1 = q ux (1+dy)                     */        \
-      v4  = one+d##Z; /* v4 = 1+dz                            */        \
-      v2  = v0*v4;    /* v2 = q ux (1-dy)(1+dz)               */        \
-      v3  = v1*v4;    /* v3 = q ux (1+dy)(1+dz)               */        \
-      v4  = one-d##Z; /* v4 = 1-dz                            */        \
-      v0 *= v4;       /* v0 = q ux (1-dy)(1-dz)               */        \
-      v1 *= v4;       /* v1 = q ux (1+dy)(1-dz)               */        \
-      v0 += v5;       /* v0 = q ux [ (1-dy)(1-dz) + uy*uz/3 ] */        \
-      v1 -= v5;       /* v1 = q ux [ (1+dy)(1-dz) - uy*uz/3 ] */        \
-      v2 -= v5;       /* v2 = q ux [ (1-dy)(1+dz) - uy*uz/3 ] */        \
-      v3 += v5;       /* v3 = q ux [ (1+dy)(1+dz) + uy*uz/3 ] */        \
-      a[offset+0] += v0;                                                \
-      a[offset+1] += v1;                                                \
-      a[offset+2] += v2;                                                \
-      a[offset+3] += v3
-
-      ACCUMULATE_J( x, y, z, 0 );
-      ACCUMULATE_J( y, z, x, 4 );
-      ACCUMULATE_J( z, x, y, 8 );
-
-      #undef ACCUMULATE_J
-    }
-
-    else                                        // Unlikely
-    {
-      local_pm->dispx = ux;
-      local_pm->dispy = uy;
-      local_pm->dispz = uz;
-
-      local_pm->i     = p - p0;
-
-      if ( move_p( p0, local_pm, a0, g, qsp ) ) // Unlikely
+      if ( nm < max_nm )
       {
-        if ( nm < max_nm )
-        {
-          pm[nm++] = local_pm[0];
-        }
+        nm++;
+        pm[ max_nm - nm ] = pm[ nm_move_p - 1 ];
+      }
 
-        else
-        {
-          itmp++;                               // Unlikely
-        }
+      else
+      {
+        itmp++;                                               // Unlikely
       }
     }
   }
-  #endif
 
-  args->seg[pipeline_rank].pm        = pm;
+  args->seg[pipeline_rank].pm        = &pm[ max_nm - nm ];
   args->seg[pipeline_rank].max_nm    = max_nm;
   args->seg[pipeline_rank].nm        = nm;
   args->seg[pipeline_rank].n_ignored = itmp;
